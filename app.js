d22ea324eda21f827a962dc608d7dfcf28c55825
gaoxiaochen <gxcsoccer@126.com>
2013-05-07 06:19:54 +0800

    update

--- app.js
+++ app.js
@@ -1,29 +1,69 @@
 var SSH = require('ssh2'),
+    fs = require('fs'),
+    path = require('path'),
+    async = require('async'),
     config = require('./config.json');
 
-var conn = new SSH();
+var conn = new SSH(),
+    baseDir = '/home/iptv/epg/tomcat/webapps/EPG/Turkcell',
+    localBase = 'D:\\gaoxiaochen\\Turkcell';
+
+fs.rmdirSync(localBase);
+
+function copyDirectory(sftp, dir, callback) {
+    console.log('opendir: ' + dir);
+    sftp.opendir(dir, function(err, handle) {
+        if (err) throw err;
+        sftp.readdir(handle, function(err, list) {
+            if (err) throw err;
+
+            var relativePath = path.relative(baseDir, dir),
+                localPath = path.resolve(localBase, relativePath);
+            if (!fs.existsSync(localPath)) {
+                fs.mkdirSync(localPath);
+            }
+
+            async.eachSeries(list || [], function(item, next) {
+                var filepath = dir + '/' + item.filename;
+
+                console.log(filepath);
+                console.log(item.longname);
+
+                if (/^(\.){1,2}?$/.test(item.filename)) {
+                    next();
+                } else if (item.longname[0] == 'd') {
+                    copyDirectory(sftp, filepath, next);
+                } else {
+                    sftp.fastGet(filepath, path.join(localPath, item.filename), function(err) {
+                        if (err) throw err;
+                        next();
+                    });
+                }
+            }, function(err) {
+                callback();
+            });
+        });
+    });
+}
+
 conn.on('connect', function() {
     console.log('Connection :: connect');
 });
 
 conn.on('ready', function() {
     console.log('Connection :: ready');
-    conn.exec('uptime', function(err, stream) {
+
+    conn.sftp(function(err, sftp) {
         if (err) throw err;
-        stream.on('data', function(data, extended) {
-            console.log((extended === 'stderr' ? 'STDERR: ' : 'STDOUT: ') + data);
-        });
-        stream.on('end', function() {
-            console.log('Stream :: EOF');
-        });
-        stream.on('close', function() {
-            console.log('Stream :: close');
-        });
-        stream.on('exit', function(code, signal) {
-            console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+        copyDirectory(sftp, baseDir, function() {
+            console.log('Transfer Complete.');
             conn.end();
         });
     });
+});
+
+conn.on('keyboard-interactive', function(name, instructions, instructionsLang, prompts, finish) {
+    finish([config.password]);
 });
 
 conn.on('error', function(err) {
